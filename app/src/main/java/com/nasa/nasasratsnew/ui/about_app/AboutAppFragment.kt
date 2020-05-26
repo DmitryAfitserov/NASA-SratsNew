b11bff19d23d6331d0f9709c8afce6b644720b26
Dmitry <dmitryafitserov@gmail.com>
2020-05-26 18:41:43 +0300

    created strings res for layout with textView for fragment about app

--- app/src/main/java/com/nasa/nasasratsnew/ui/about_app/AboutAppFragment.kt
+++ app/src/main/java/com/nasa/nasasratsnew/ui/about_app/AboutAppFragment.kt
@@ -1,6 +1,7 @@
 package com.nasa.nasasratsnew.ui.about_app
 
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +10,9 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.nasa.nasasratsnew.R
+import android.content.Intent
+import android.net.Uri
+
 
 class AboutAppFragment : Fragment() {
 
@@ -23,6 +27,27 @@ class AboutAppFragment : Fragment() {
             ViewModelProviders.of(this).get(AboutAppViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_about_app, container, false)
 
+        val textViewAppInMarket = root.findViewById<TextView>(R.id.app_in_market)
+        textViewAppInMarket.setOnClickListener {
+
+            val appPackageName = context!!.packageName // getPackageName() from Context or Activity object
+            try {
+                startActivity(
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("market://details?id=$appPackageName")
+                    )
+                )
+            } catch (anfe: android.content.ActivityNotFoundException) {
+                startActivity(
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("https://play.google.com/store/apps/details?id=$appPackageName")
+                    )
+                )
+            }
+
+        }
 
 //        val textView: TextView = root.findViewById(R.id.text_share)
 //        shareViewModel.text.observe(this, Observer {
